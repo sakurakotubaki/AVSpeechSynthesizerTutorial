5f4f135c5e331e04191467335c61f3818946c26d
Jboy422 <gitonly543@gmail.com>
2024-08-12 20:01:29 +0900

    Initial Commit

---
+++ AVSpeechSynthesizerTutorial/AVSpeechSynthesizerTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  AVSpeechSynthesizerTutorialApp.swift
+//  AVSpeechSynthesizerTutorial
+//
+//  Created by Jboy422 on 2024/08/12.
+//
+
+import SwiftUI
+
+@main
+struct AVSpeechSynthesizerTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
